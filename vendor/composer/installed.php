e68096995a4faec0c28e39443e158da09908b18a
adityasukmaa <adityasukma67@gmail.com>
2024-05-10 22:32:34 +0700

    penambahan landingpage

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3b485ff1c160e4fbf284194571b428f8a8589060',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3b485ff1c160e4fbf284194571b428f8a8589060',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
